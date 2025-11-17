8efe6dc41e4bd8d6b538866b0d7e0eca7bb36d42
Jo Hallett <joseph.hallett@bristol.ac.uk>
2025-11-17 13:35:11 +0000

    Updating for tomorrows class

--- 07-debugging/lab/cheater.sh
+++ 07-debugging/lab/cheater.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-gmake -B all
+command -v gmake && gmake -B all
+command -v make && make -B all
 
 printf "Crackme 1: "
 >/dev/null ./crackme-1 <<<"Beetlejuice" && printf "PASS\n" || printf "FAIL\n";
@@ -8,7 +9,7 @@ printf "Crackme 2: "
 printf "Crackme 3: "
 >/dev/null ./crackme-3 <<<"MeSayDay0h" && printf "PASS\n" || printf "FAIL\n";
 printf "Crackme 4: "
->/dev/null ./crackme-4 <<<"12345" && printf "PASS\n" || printf "FAIL\n";
+>/dev/null ./crackme-4 <<<"12345" && printf "PASS\n" || printf "FAIL (but thats kinda expected)\n";
 printf "Crackme 5: "
 >/dev/null ./crackme-5 <<<"z+dddddd++>z+dd+dd+++++d+>c>c<+++++++d>>c>c>c>c>c>c<<<<<<>-------------d>cl----->>++++++++d->---------d->" && printf "PASS\n" || printf "FAIL\n";
  
